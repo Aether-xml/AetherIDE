0d085e773547ab9cfb8459a1dbdc00ad55e6e370
Aether-xml <kurumsalhesapbtw@gmail.com>
2026-02-16 15:51:21 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'aetheride-v1';
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/css/main.css',
+    '/css/sidebar.css',
+    '/css/editor.css',
+    '/css/chat.css',
+    '/css/modal.css',
+    '/css/responsive.css',
+    '/js/utils.js',
+    '/js/storage.js',
+    '/js/themes.js',
+    '/js/api.js',
+    '/js/editor.js',
+    '/js/chat.js',
+    '/js/settings.js',
+    '/js/app.js',
+    '/js/modes/direct.js',
+    '/js/modes/planner.js',
+    '/js/modes/team.js',
+];
+
+self.addEventListener('install', e => {
+    e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request).then(r => r || fetch(e.request))
+    );
+});
